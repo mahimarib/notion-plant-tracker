bc1c0df705dbd7b636160c534658546fdc53f03d
Mahim <mahim216@gmail.com>
2022-08-04 06:11:48 -0400

    fixing type for params

--- src/notion-client/waterLog.ts
+++ src/notion-client/waterLog.ts
@@ -10,42 +10,40 @@ function getWateringMethodID(method: WateringMethod) {
 }
 
 export function addToLog(id: string, date: string, method: WateringMethod) {
-    const properties: Record<string, any> = {
-        [waterLog.nameID]: {
-            title: [
-                {
-                    type: 'mention',
-                    mention: {
-                        type: 'page',
-                        page: { id },
-                    },
-                },
-                {
-                    type: 'text',
-                    text: {
-                        content: ' ',
-                    },
-                },
-            ],
-        },
-        [waterLog.dateID]: {
-            date: {
-                start: date,
-            },
-        },
-        [waterLog.method.id]: {
-            select: {
-                id: getWateringMethodID(method),
-            },
-        },
-    }
-
     return limiter.schedule(() =>
         notion.pages.create({
             parent: {
-                database_id: waterLog.id
+                type: 'database_id',
+                database_id: waterLog.id,
             },
-            properties: properties
+            properties: {
+                [waterLog.nameID]: {
+                    title: [
+                        {
+                            type: 'mention',
+                            mention: {
+                                page: { id }
+                            }
+                        },
+                        {
+                            type: 'text',
+                            text: {
+                                content: ' ',
+                            },
+                        }
+                    ]
+                },
+                [waterLog.dateID]: {
+                    date: {
+                        start: date
+                    }
+                },
+                [waterLog.method.id]: {
+                    select: {
+                        id: getWateringMethodID(method)
+                    }
+                }
+            }
         })
     );
 }
